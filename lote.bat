3d382cf7325bc46ab7e2a7665c10211bfe135ea5
arianaburga <56941154+arianaburga@users.noreply.github.com>
2019-11-16 11:10:15 -0600

    se agrego la tercera condicional simple

---
+++ lote.bat
@@ -0,0 +1,30 @@
+python burga06/simples/ejercicio1.py "juana" "cereal" 3.20 4
+python burga06/simples/ejercicio2.py "marco" 12 6.90
+python burga06/simples/ejercicio3.py "miguel" 15.06 12.60 16.70
+python burga06/simples/ejercicio4.py "maria" 12.30 32.60 40.50
+python burga06/simples/ejercicio5.py "luis" 12 6.50
+python burga06/simples/ejercicio6.py "martina" 80.90 120.60
+python burga06/simples/ejercicio7.py "luis" 12 10.20 6.90
+python burga06/simples/ejercicio8.py "luis_miguel" 3 8.60 11.80
+python burga06/simples/ejercicio9.py "maricielo" 50 80.60 40.30
+python burga06/simples/ejercicio10.py "juan" 23 15
+python burga06/dobles/ejercicio11.py "juana" "cereal" 3.20 4
+python burga06/dobles/ejercicio12.py "marco" 12 6.90
+python burga06/dobles/ejercicio13.py "miguel" 15.06 12.60 16.70
+python burga06/dobles/ejercicio14.py "maria" 12.30 32.60 40.50
+python burga06/dobles/ejercicio15.py "luis" 12 6.50
+python burga06/dobles/ejercicio16.py "martina" 80.90 120.60
+python burga06/dobles/ejercicio17.py "luis" 12 10.20 6.90
+python burga06/dobles/ejercicio18.py "luis_miguel" 3 8.60 11.80
+python burga06/dobles/ejercicio19.py "maricielo" 50 80.60 40.30
+python burga06/dobles/ejercicio20.py "juan" 23 15
+python burga06/multiples/ejercicio21.py "juana" "cereal" 3.20 4
+python burga06/multiples/ejercicio22.py "marco" 12 6.90
+python burga06/multiples/ejercicio23.py "miguel" 15.06 12.60 16.70
+python burga06/multiples/ejercicio24.py "maria" 12.30 32.60 40.50
+python burga06/multiples/ejercicio25.py "luis" 12 6.50
+python burga06/multiples/ejercicio26.py "martina" 80.90 120.60
+python burga06/multiples/ejercicio27.py "luis" 12 10.20 6.90
+python burga06/multiples/ejercicio28.py "luis_miguel" 3 8.60 11.80
+python burga06/multiples/ejercicio29.py "maricielo" 50 80.60 40.30
+python burga06/multiples/ejercicio30.py "juan" 23 15
